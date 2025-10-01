7ac78584e99db1395da06e73a2347e4a1df9aa74
UnknownHumanBean <maambrosehyde5414@student.pps.net>
2025-10-01 09:49:58 -0700

    I did a thing

--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
+var speed = 300
+var direction : Vector2
+var target
+func set_direction(target):
+	direction = position.direction_to(target)
+	pass
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
+func _physics_process(delta: float):
+	position += direction * speed * delta
 func _on_body_entered(body):
+	
+	
 	pass
--- scripts/turret.gd
+++ scripts/turret.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+var projectile_original = preload("res://scenes/enemy_projectile.tscn")
+var speed = 300
 
-func _ready():
+func _ready(body):
 	
 	pass
 
@@ -13,9 +15,23 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		
+	 # Replace with function body.
+# TODO: Create a new projectile instance
+		var projectile_clone = projectile_original.instantiate()
 	
-	pass # Replace with function body.
-
+	# TODO: Set projectile position to player position
+		projectile_clone.global_position = position
+	
+	# TODO: Set projectile direction using facing variable
+		projectile_clone.set_direction(body.position)
+	# TODO: Add projectile to the game world
+		get_tree().get_root().add_child(projectile_clone)
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	pass # Replace with function body.
+
+	
+
+	pass
