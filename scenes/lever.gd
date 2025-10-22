643b585c681ab7cd7a456a5da2bb3b11f97e38e1
UnknownHumanBean <maambrosehyde5414@student.pps.net>
2025-10-22 09:31:26 -0700

    Shoul be working, game doesnt load though.

--- scenes/lever.gd
+++ scenes/lever.gd
@@ -4,8 +4,10 @@ var on = false
 var player
 var in_range = false
 var number_on = 0
+var body
 @onready var _animation_lever: AnimatedSprite2D = $AnimatedSprite2D
 
+@warning_ignore("shadowed_variable")
 func on_body_entered(body):
 	if body == "Player":
 		in_range = true
@@ -14,12 +16,13 @@ func _process(_float) -> void:
 	if in_range == true and Input.is_action_just_pressed("ui_select"):
 		number_on += 1
 		on = true
+		print (str(number_on))
 	
 	if on == true:
 		update_animation()
 		
-	#if number_on == 3:
-		#Do something
+	if number_on == 3:
+		body.change_coins(1)
 		
 func update_animation():
 	_animation_lever.play("res://sprites/items/ALever.png")
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var _animation_player: AnimatedSprite2D = $AnimatedSprite2D
 var projectile_original = preload("res://scenes/projectile.tscn")
+@onready var melee_area: Area2D = $MeleeArea
 
 var xSpeed = 300.0
 var xDirection = 0
@@ -9,15 +10,14 @@ var ySpeed = 300.0
 var yDirection = 0
 var coins = 0
 var is_atking = false
-var atk_timer = 0.6
+var atk_timer = 0.0
 @export var offset : Vector2 = Vector2(0, -25)
 @onready var melee_box : CollisionShape2D = $Area2D/CollisionShape2D2
 var in_range = false
 var maxHealth = 100
 var health = maxHealth
 var atk_cooldown = 0.6
-var body
-var mana_gain_amount = 0.01
+var mana_gain_amount = 1
 var mana_amount = 0
 var max_mana = 100
 var max_energy = 100
@@ -33,10 +33,25 @@ var sat_use_speed = 1
 var nat_regen_speed = 1
 var exhaustion = 1
 var moving = false
+var rng := RandomNumberGenerator.new()
+var n = rng.randi_range(1, 10)
+var f = rng.randf()
+var f2 = rng.randf_range(2.5, 7.0)
+var attack_damage = 25
+var attack_hit_done = false
 
 func _ready() -> void:
 	energy_amount = max_energy
 	saturation = max_saturation
+	rng.randomize()
+	if not InputMap.has_action("attack"):
+		InputMap.add_action("attack")
+	InputMap.action_erase_events("attack")
+	var ev := InputEventKey.new()
+	ev.keycode = KEY_E
+	InputMap.action_add_event("attack", ev)
+	melee_area.monitoring = true
+	melee_area.set_deferred("monitorable", true)
 	pass
 
 func _physics_process(_delta):
@@ -66,7 +81,7 @@ func _physics_process(_delta):
 			print ("Mana " + str(mana_amount))
 			
 		elif mana_amount <= 10 * mana_tax:
-			mana_amount += 0 + mana_gain_amount
+			mana_amount += mana_gain_amount
 			print ("Mana " + str(mana_amount))
 	# call the animation function
 	update_animation()
@@ -75,45 +90,17 @@ func _physics_process(_delta):
 	# This is a special Godot function that makes the movement happen
 	move_and_slide()
 
-	if Input.is_action_just_pressed("ui_text_delete"):
-		is_atking = true
-		atk_timer = atk_cooldown
-		attack(body)
+	if Input.is_action_just_pressed("attack"):
+		start_attack()
 		
 	if is_atking:
 		atk_timer -= _delta
 		if atk_timer <= 0:
 			is_atking = false
-			atk_timer = 0.6
-		
-# TODO: Create animation function (add this outside of _physics_process)
-func update_animation():
-	# TODO: Set the animation based on the facing direction
-	if velocity.is_zero_approx():
-		_animation_player.play("idle_" + facing)
-	# This combines "idle_" with whatever direction we're facing
-		pass
-	elif !velocity.is_zero_approx():
-		#walking animation here
-		_animation_player.play("walk_" + facing)
-		
-	if is_atking:
-		_animation_player.play("attack_"+ facing)
-		
-		
-		pass
-		
-	
-
-
-# TODO: Create health change function for interactions
-func change_health(_amount:int):
-		health += _amount
-		mana_tax *= 1.1
-		if health <= 0:
-			die()
-		if health > maxHealth:
-			health = maxHealth
+			attack_hit_done = false
+			
+		update_animation()
+		move_and_slide()
 		
 
 func change_coins(_amount:int):
@@ -131,8 +118,6 @@ func change_coins(_amount:int):
 		sat_use_speed = sat_use_speed * 2
 		nat_regen_speed = nat_regen_speed * 2
 		
-func die(): 
-	queue_free()
 	
 # TODO: Create shooting function
 func shoot():
@@ -155,11 +140,41 @@ func on_body_entered(body):
 	if body.is_in_group("enemies"): 
 		var _in_range = true
 		
-@warning_ignore("shadowed_variable")
-func attack(body):
-	if in_range == true and body.is_in_group("enemies") and energy_amount >= (10 * exhaustion):
-		energy_amount -= 10 * exhaustion
-		print ("hit")
+func start_attack() -> void:
+	# Start an attack, run the hit once immediately
+	is_atking = true
+	atk_timer = atk_cooldown
+	if not attack_hit_done:
+		do_melee_attack()
+		attack_hit_done = true
+		
+func do_melee_attack() -> void:
+	# Immediately damage every overlapping enemy (simple instant hit)
+	var bodies = melee_area.get_overlapping_bodies()
+	for body in bodies:
+		if body and body.is_in_group("enemies"):
+			if body.has_method("take_damage"):
+				body.take_damage(attack_damage)
+			elif body.has_method("change_health"):
+				body.change_health(attack_damage)
+				
+func update_animation():
+	if velocity.length() < 1:
+		_animation_player.play("idle_" + facing)
+	else:
+		_animation_player.play("walk_" + facing)
+		
+func change_health(_amount:int) -> void:
+	health += _amount
+	if health < 1:
+		die()
+	if health > maxHealth:
+		health = maxHealth
+	print("Health: ", health)
+	
+func die() -> void:
+	print("WASTED!")
+	queue_free()
 		
 func _process(_delta: float) -> void:
 	if saturation >= 1 and health < 100:
@@ -175,27 +190,27 @@ func _process(_delta: float) -> void:
 	if energy_amount >= max_energy:
 		energy_amount = max_energy
 		
-	if Input.get_axis("ui_left", "ui_right") and energy_amount >= 0.01:
+	if Input.get_axis("ui_left", "ui_right") and energy_amount >= 1:
 		xSpeed = 300 * speed_mult
 		ySpeed = 300 * speed_mult
 		energy_amount -= 0.25 * exhaustion
 		print("Energy " ,str(energy_amount))
 		moving = true
-	elif Input.get_axis("ui_left", "ui_right") and energy_amount <= 0.01:
-		xSpeed = 200 * speed_mult
-		ySpeed = 200 * speed_mult
+	elif Input.get_axis("ui_left", "ui_right") and energy_amount <= 1:
+		xSpeed = 50 * speed_mult
+		ySpeed = 50 * speed_mult
 		moving = false
 		print("Energy " ,str(energy_amount))
 		
-	if Input.get_axis("ui_up", "ui_down") and energy_amount >= 0.01:
+	if Input.get_axis("ui_up", "ui_down") and energy_amount >= 1:
 		xSpeed = 300 * speed_mult
 		ySpeed = 300 * speed_mult
 		energy_amount -= 0.25 * exhaustion
 		print("Energy " ,str(energy_amount))
 		moving = true
-	elif Input.get_axis("ui_up", "ui_down") and energy_amount <= 0.01:
-		xSpeed = 200 * speed_mult
-		ySpeed = 200 * speed_mult
+	elif Input.get_axis("ui_up", "ui_down") and energy_amount <= 1:
+		xSpeed = 50 * speed_mult
+		ySpeed = 50 * speed_mult
 		moving = false
 		print("Energy " ,str(energy_amount))
 		
