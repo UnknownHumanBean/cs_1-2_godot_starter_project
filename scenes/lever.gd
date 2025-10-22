bb698c9c597bb5b7be51b7787e5f87d0606a4fd3
UnknownHumanBean <maambrosehyde5414@student.pps.net>
2025-10-22 09:46:24 -0700

    levers now update, game doesnt run though.

--- scenes/lever.gd
+++ scenes/lever.gd
@@ -1,29 +1,47 @@
 extends Area2D
 
-var on = false
-var player
-var in_range = false
-var number_on = 0
-var body
-@onready var _animation_lever: AnimatedSprite2D = $AnimatedSprite2D
+@export var lever_name: String = "lever_1"
+var in_range: bool = false
+var is_on: bool = false
 
-@warning_ignore("shadowed_variable")
-func on_body_entered(body):
-	if body == "Player":
+signal toggled(name, state)
+
+func _ready() -> void:
+	add_to_group("levers")
+	# ensure monitoring is on so Area2D signals work
+	monitoring = true
+
+func _on_Lever_body_entered(body: Node) -> void:
+	# detect player; you can also check is_in_group("players") if you add the player to that group
+	if body.name == "Player" or body.is_in_group("player"):
 		in_range = true
-		
-func _process(_float) -> void:
-	if in_range == true and Input.is_action_just_pressed("ui_select"):
-		number_on += 1
-		on = true
-		print (str(number_on))
-	
-	if on == true:
-		update_animation()
-		
-	if number_on == 3:
-		body.change_coins(1)
-		
-func update_animation():
-	_animation_lever.play("res://sprites/items/ALever.png")
-	
+
+func _on_Lever_body_exited(body: Node) -> void:
+	if body.name == "Player" or body.is_in_group("player"):
+		in_range = false
+
+func _process(_delta: float) -> void:
+	# Press E / "attack" while in range to toggle
+	if in_range and Input.is_action_just_pressed("attack"):
+		toggle()
+
+func toggle() -> void:
+	is_on = not is_on
+	if has_node("AnimatedSprite2D"):
+		if has_node("AnimatedSprite2D"):
+		# play the "on" or "off" animation (Godot 4 ternary style)
+			$AnimatedSprite2D.play("on" if is_on else "off")
+	elif has_node("Sprite"):
+		# assume Sprite2D with two frames: 0 = off, 1 = on
+		if $Sprite is Sprite2D and $Sprite.frames:
+			$Sprite.frame = 1 if is_on else 0
+
+	emit_signal("toggled", lever_name, is_on)
+
+	# If you have a global GameManager autoload, call a handler if it exists
+	if Engine.has_singleton("GameManager"):
+		# In Godot autoloads are available as global variables named by the autoload key.
+		# Try calling GameManager.on_lever_toggled if it exists.
+		if GameManager and GameManager.has_method("on_lever_toggled"):
+			GameManager.on_lever_toggled(lever_name, is_on)
+			
